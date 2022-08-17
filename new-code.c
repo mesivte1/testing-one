3536b4906e9eaaf2b8e8c041c56ee99d934090fa
Steve bob <mesivte1@gmail.com>
2022-08-17 18:03:55 -0400

    new test

---
+++ new-code.c
@@ -0,0 +1,10 @@
+#include <Stdio.h>
+
+
+int main()
+{
+    int a,b;
+    printf("Enter a number to get it squared.\n");
+    scanf("%d", &a);
+    printf("Here is your number squared = %d \n", a = a * a);
+}
